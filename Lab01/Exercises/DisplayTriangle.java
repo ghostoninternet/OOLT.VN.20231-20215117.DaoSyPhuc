6250edc1df7bf020ca57edded60a838984ef214d
ghostoninternet <daosyphuc288@gmail.com>
2023-10-13 21:37:34 +0700

    Add exercise 3: Write a program to display a triangle with a height of n stars (*), n is entered by users

---
+++ Lab01/Exercises/DisplayTriangle.java
@@ -0,0 +1,23 @@
+import javax.swing.JOptionPane; 
+
+public class DisplayTriangle {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(JOptionPane.showInputDialog(
+                            null, 
+                            "Enter the triangle height: ", 
+                            "Triangle", 
+                            JOptionPane.INFORMATION_MESSAGE));
+
+        String message = "";
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n-i; j++) {
+                message += " ";
+            }
+            for (int k = 1; k <= 2*i - 1; k++) {
+                message += "*";
+            }
+            message += "\n";
+        }
+        System.out.println(message);
+    }
+}
