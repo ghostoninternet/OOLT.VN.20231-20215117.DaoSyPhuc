546e6c7f9654af6e0c5892a9b8bcbae093d60eb7
ghostoninternet <daosyphuc288@gmail.com>
2023-11-25 22:17:52 +0700

    Implementation of CompactDisc class

---
+++ AimsProject/src/hust/soict/hedspi/aims/media/CompactDisc.java
@@ -0,0 +1,65 @@
+package hust.soict.hedspi.aims.media;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class CompactDisc extends Disc {
+	private static int nbCompactDiscs = 0;
+	
+	private String artist;
+	private List<Track> tracks = new ArrayList<Track>();
+	
+	public CompactDisc(int id, String title, String artist) {
+		super(id, title);
+		this.artist = artist;
+		nbCompactDiscs++;
+	}
+
+	public CompactDisc(int id, String title, String artist, String category, float cost) {
+	    super(id, title, category, cost);
+	    this.artist = artist;
+	    nbCompactDiscs++;
+	}
+	    
+	public CompactDisc(int id, String title, String artist, String category, float cost, String director) {
+		super(id, title, category, cost, director);
+		this.artist = artist;
+		nbCompactDiscs++;
+	}
+
+	public CompactDisc(int id, String title, String artist, String category, float cost, String director, int length) {
+		super(id, title, category, cost, director, length);
+		this.artist = artist;
+		nbCompactDiscs++;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+	
+	public void addTrack(Track track) {
+		if(!tracks.contains(track)) {
+			tracks.add(track);
+			System.out.printf("Successfully added a track with title %s and length %d to tracks list!", track.getTitle(), track.getLength());
+		} else {
+			System.out.printf("Track with title %s and length %d is already existed!", track.getTitle(), track.getLength());
+		}
+	}
+	
+	public void removeTrack(Track track) {
+		if(tracks.contains(track)) {
+			tracks.remove(track);
+			System.out.printf("Successfully removed a track with title %s and length %d out of tracks list!", track.getTitle(), track.getLength());
+		} else {
+			System.out.printf("Track with title %s and length %d is not existed!", track.getTitle(), track.getLength());
+		}
+	}
+	
+	public int getLength() {
+		int sumLength = 0;
+		for (Track track: tracks) {
+			sumLength += track.getLength();
+		}
+		return sumLength;
+	}
+}
