31ced74325d6a34324c18704b57db921991313ba
ghostoninternet <daosyphuc288@gmail.com>
2023-11-26 01:38:45 +0700

    Update Cart and Store class to work with Media

--- AimsProject/src/hust/soict/hedspi/aims/store/Store.java
+++ AimsProject/src/hust/soict/hedspi/aims/store/Store.java
@@ -1,48 +1,29 @@
 package hust.soict.hedspi.aims.store;
 
-import hust.soict.hedspi.aims.media.DigitalVideoDisc;
+import java.util.ArrayList;
+import java.util.List;
+
+import hust.soict.hedspi.aims.media.Media;
 
 public class Store {
-	public static final int MAXIMUM_STORE_ITEM = 10000;
-	private int currentQuantityInStore = 0;
-	private DigitalVideoDisc itemsInStore[] = new DigitalVideoDisc[MAXIMUM_STORE_ITEM];
 	
-	public void addDVD(DigitalVideoDisc dvd) {
-		itemsInStore[currentQuantityInStore] = dvd;
-		currentQuantityInStore++;
-		System.out.println("Successfully added a DVD to store");
+	private List<Media> itemsInStore = new ArrayList<Media>();
+	
+	public void addMedia(Media media) {
+		itemsInStore.add(media);
 	}
 	
-	public void addDVD(DigitalVideoDisc[] dvdList) {
-		int j = 0;
-		while(currentQuantityInStore <= MAXIMUM_STORE_ITEM) {
-			itemsInStore[currentQuantityInStore] = dvdList[j];
-			j++;
-			currentQuantityInStore++;
+	public void addMedia(List<Media> listMedia) {
+		for(Media item: listMedia) {
+			itemsInStore.add(item);
 		}
-		if (j+1 == dvdList.length) System.out.println("Successfully added a list of DVDs to store");
-		else System.out.printf("Can only added %d DVD(s) out of %d DVD(s) because the store is now full!", j+1, dvdList.length);
 	}
 	
-	public void removeDVD(DigitalVideoDisc dvd) {
-		if (currentQuantityInStore == 0) {
-			System.out.println("Store is empty! No items can be removed");
+	public void removeMedia(Media media) {
+		if (itemsInStore.remove(media)) {
+			System.out.println("Successfully removed a media out of store");
 		} else {
-			int deletedPosition = 0;
-			for(int i = 0; i < MAXIMUM_STORE_ITEM; i++) {
-				if(itemsInStore[i] != null) {
-					if (itemsInStore[i].getTitle().equals(dvd.getTitle())) {
-						itemsInStore[i] = null;
-						currentQuantityInStore--;
-						deletedPosition = i;
-						System.out.println("Removed disc " + dvd.getTitle() + " out of the store");
-						break;
-					}
-				}
-			}
-			for(int j = deletedPosition; j <= currentQuantityInStore; j++) {
-				itemsInStore[j] = itemsInStore[j+1];
-			}
+			System.out.println("Media isn't existed in store");
 		}
 	}
 }
